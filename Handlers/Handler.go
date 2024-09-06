9fc7cf7e53750daf6c415fc15e45afdd74194561
Abdelkabir <abdelkabir.soudri@gmail.com>
2024-09-06 20:24:43 +0100

    sample update in code

--- Handlers/Handler.go
+++ Handlers/Handler.go
@@ -2,22 +2,17 @@ package groupie
 
 import (
 	"encoding/json"
-	"fmt"
-	"io"
 	"log"
 	"net/http"
-	"os"
-
+	"io"
 	groupie "groupie/data"
 )
 
 func Handler(url string, data []groupie.Band) []groupie.Band {
 	response, err := http.Get(url)
 	if err != nil {
-		fmt.Print(err.Error())
-		os.Exit(1)
+		log.Fatal(err.Error())
 	}
-
 	responseData, err := io.ReadAll(response.Body)
 	if err != nil {
 		log.Fatal(err)
